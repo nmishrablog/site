980a40379b74de9ff1f68ea497aaaabc5783c2ba
nmishrablog <31518505+nmishrablog@users.noreply.github.com>
2019-11-27 13:39:48 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const IS_PROD = process.env.NODE_ENV === 'production'
 
 export const PUBLIC_STRIPE_KEY = IS_PROD
   ? 'pk_test_yCLJq54TurIhrSBJsK2H2y4G'
-  : 'pk_test_eU1Cn0yK7HTAPZeXjA0uwWL8'
+  : 'pk_test_yCLJq54TurIhrSBJsK2H2y4G'
